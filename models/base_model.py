1d0d7898848f8146a958e937a47e6eb97db556fc
THOSSYN <mtafortunefinder@gmail.com>
2023-08-08 08:47:48 -0700

    Updated: models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -24,3 +24,14 @@ class BaseModel:
     def __str__(self):
         """ Prints a string reprsentation of instance """
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+
+    def save(self):
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return self.updated_at
+
+    def to_dict(self):
+        """Returns a dictionary of the attributes of BaseModel class"""
+        res_dict = {}
+        for key, value in self.__dict__:
+            res_dict[key] = value
+        return res_dict
