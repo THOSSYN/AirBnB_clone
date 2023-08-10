c1bb84f4f89f1f19a97e2fd6f9bd03a6d062a3fc
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-10 12:01:30 +0000

    Implemented the show method of the console

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ the command line interpreter"""
 
 import cmd
 from models.base_model import BaseModel
-
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """Creates a class that handles the cmd console"""
@@ -18,9 +18,42 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         elif line == "BaseModel":
             obj = BaseModel()
+            obj.save()
             print(obj.id)
         else:
             print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """ The show method displays the string representation of
+        a basemodel based on class name and id """
+
+        # Argument in args is a tuple, taking all argument
+        # as a string, this is why you have to split it
+        # by the delimiter used to enter the arguments
+        args = args.split()
+
+        if not args:
+            print("** class name missing **")
+            return
+
+        if args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+
+        if len(args) != 2:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[1]
+        storage.reload()
+        inst_dict = storage.all()
+
+        for key, value in inst_dict.items():
+            if obj_id == value.id:
+                print(value)
+                return
+
+        print("** no instance found **")
 
     def emptyline(self):
         """ The emptyline method called when an empty line
